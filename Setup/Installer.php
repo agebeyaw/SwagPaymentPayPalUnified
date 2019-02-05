2cc008e12c367b41c82ebcd4ed6b7a3a38dec8d2
aneteneh <antenehgeb@gmail.com>
2019-02-05 21:19:47 +0100

    change paypal urls with en lang code

--- Setup/Installer.php
+++ Setup/Installer.php
@@ -223,7 +223,7 @@ class Installer
     {
         return '<!-- PayPal Logo -->'
         . '<a onclick="window.open(this.href, \'olcwhatispaypal\',\'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=500\'); return false;"'
-        . ' href="https://www.paypal.com/en/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside" target="_blank">'
+        . ' href="https://www.paypal.com/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside" target="_blank">'
         . '<img src="{link file=\'frontend/_public/src/img/sidebar-paypal-generic.png\' fullPath}" alt="Logo \'PayPal recommended\'">'
         . '</a><br>' . '<!-- PayPal Logo -->';
     }
@@ -242,7 +242,7 @@ class Installer
             [
                 'description' => 'PayPal',
                 'additionalDescription' => '<!-- PayPal Logo --><a onclick="window.open(this.href, \'olcwhatispaypal\',\'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=500\'); '
-                    . 'return false;" href="https://www.paypal.com/en/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside" target="_blank">'
+                    . 'return false;" href="https://www.paypal.com/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside" target="_blank">'
                     . '<img src="{link file=\'frontend/_public/src/img/sidebar-paypal-generic.png\' fullPath}" alt="Logo \'PayPal recommended\'">'
                     . '</a><br><!-- PayPal Logo -->Paying with PayPal - easy, fast and secure.',
             ],
